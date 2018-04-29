fad34be5be5378a851780e469cc7ab3f00405b68
Jérémy Da Trindade <admin@macbook-pro-de-jeremy.home>
2018-04-29 19:42:54 +0200

    mudando o posicionamento do texto

--- GD/exemplo-03.php
+++ GD/exemplo-03.php
@@ -10,11 +10,11 @@ $titleColor = imagecolorallocate($image, 0,0,0);
 
 $grey = imagecolorallocate($image, 100,100,100);
 
-imagettftext ($image, 32,0,450,150, $titleColor, $font["Bevan"],"CERTIFICADO");
+imagettftext ($image, 32,0,320,250, $titleColor, $font["Bevan"],"CERTIFICADO");
 
-imagettftext($image, 32,0,450,350, $titleColor, $font["Playball"], "Divaney Aparecido");
+imagettftext($image, 32,0,375,350, $titleColor, $font["Playball"], "Divaney Aparecido");
 
-imagestring($image,5,450,350, utf8_decode("Concluído em: ".date("d/m/Y")), $grey);
+imagestring($image,5,440,370, utf8_decode("Concluído em: ".date("d/m/Y")), $grey);
 
 header("Content-type: image/jpg");
 
