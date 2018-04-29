1c627b9d5efc55cf9e2da5124a765f7b50808389
Jérémy Da Trindade <admin@macbook-pro-de-jeremy.home>
2018-04-29 19:39:04 +0200

    ttf bug corrigido com ajuda de superglobal __DIR__

--- GD/exemplo-03.php
+++ GD/exemplo-03.php
@@ -1,21 +1,27 @@
 <?php
 
+//Array com os nomes e caminhos das Fontes
+$font = array("Bevan"=>__DIR__.DIRECTORY_SEPARATOR."fonts".DIRECTORY_SEPARATOR. "Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf",
+"Playball"=>__DIR__.DIRECTORY_SEPARATOR."fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf");
+
 $image = imagecreatefromjpeg("certificado.jpg");
 
-$titleColor = imagecolorallocate($image, 0, 0, 0);
+$titleColor = imagecolorallocate($image, 0,0,0);
 
-$gray = imagecolorallocate($image,100,100,100);
+$grey = imagecolorallocate($image, 100,100,100);
 
-imagettftext($image, 32, 0,450, 150, $titleColor,"fonts/Bevan/Bevan-Regular.ttf", "CERTIFICADO");
+imagettftext ($image, 32,0,450,150, $titleColor, $font["Bevan"],"CERTIFICADO");
 
-imagettftext($image, 32, 0,440, 350, $titleColor, "Divanei Aprecido");
+imagettftext($image, 32,0,450,350, $titleColor, $font["Playball"], "Divaney Aparecido");
 
-imagettftext($image, 32, 0,440, 370, $titleColor,"fonts/Playball/Playball-Regular.ttf", utf8_decode("Concluído em: ").date("d/m/Y"));
+imagestring($image,5,450,350, utf8_decode("Concluído em: ".date("d/m/Y")), $grey);
 
-//header("Content-type: image/jpeg");
+header("Content-type: image/jpg");
 
 imagejpeg($image);
 
 imagedestroy($image);
 
 ?>
+
+
