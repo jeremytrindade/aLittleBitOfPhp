523d76df0d2b7bf1d82907546267263dff19d58f
Jérémy Da Trindade <admin@macbook-pro-de-jeremy.home>
2018-05-01 20:28:35 +0200

    Inicio a Segurança no PHP

--- website/index.php
+++ website/index.php
@@ -5,7 +5,9 @@ require_once("vendor/autoload.php");
 $app = new \Slim\Slim();
 
 $app->get('/', function () {
-    echo "Home Page";
+    echo json_encode(array(
+        "date"=>date("Y-m-d H:i:s")
+    ));
 });
 $app->get('/hello/:name', function ($name) {
     echo "Hello, " . $name;
